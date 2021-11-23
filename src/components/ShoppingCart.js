5d3c292c5923d8a312f2539bdfa8d86b66964300
lucastrann <71780014+lucastrann@users.noreply.github.com>
2021-11-23 23:39:29 +0100

    Update ShoppingCart.js

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ShoppingItems from './ShoppingItems';
 import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
 
 export default ({ close }) => (
-    
+
   <div className="menu">
-      <ShoppingItems></ShoppingItems>
-      <Link to="/Checkout">
-        <button>Bestill</button>
-      </Link>
+    <ShoppingItems></ShoppingItems>
+    <Link to="/Checkout">
+      <Button className="food-button" variant="contained">
+        <strong>Bestill</strong>
+      </Button>
+    </Link>
   </div>
 );
