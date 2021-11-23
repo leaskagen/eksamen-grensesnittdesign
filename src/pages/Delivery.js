e9b86587bbe0bd3d089bd90fadf4c2542ef8e096
leaskagen <50326406+leaskagen@users.noreply.github.com>
2021-11-23 18:59:21 +0100

    Delivery page desktop

--- src/pages/Delivery.js
+++ src/pages/Delivery.js
@@ -9,24 +9,25 @@ export default function Delivery() {
             <SideBar pageWrapId={'page-wrap'} outerContainerId={'outer-container'}></SideBar>
             <NavBar></NavBar>
             <main id="deliveryContent">
-                <div>
-                <h1>Leveringsområder</h1>
-                <hr></hr>
-                <p>Vi leverer til alle adresser i Oslo med unntak av Sørbråten, Sørkedalen og Maridalen</p>
-                <p>Leveringstillegg: 69kr</p>
-                <p>Adressen vår:</p>
-                <ul>
-                    <li>Louises Gate 17c</li>
-                    <li>0169 Oslo</li>
-                </ul>
-                    <div class="mapouter"><div class="gmap_canvas">
-                        <iframe width="300" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q=louises%20gate%2017c&t=&z=14&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-                        </div>
+                <div id="delivery-div" className="desktop-div">
+                    <div id="delivery-h">
+                        <h1>Leveringsområder</h1>
+                        <hr></hr>
+                    </div>
+                    <div id="delivery-text">
+                        <p>Vi leverer til alle adresser i Oslo med unntak av Sørbråten, Sørkedalen og Maridalen</p>
+                        <p>Leveringstillegg: 69kr</p>
+                        <p>Adressen vår:</p>
+                        <ul>
+                            <li>Louises Gate 17c</li>
+                            <li>0169 Oslo</li>
+                        </ul>
                     </div> 
-                
+                    <div class="mapouter"><div class="gmap_canvas">
+                        <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=louises%20gate%2017c&t=&z=14&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div>
+                    </div>
                 </div>
             </main>
         </div>
-        
     )
 }
