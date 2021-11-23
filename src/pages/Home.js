9a571cd4507eda23d635420dc9c6ed19c89bbafd
leaskagen <50326406+leaskagen@users.noreply.github.com>
2021-11-23 13:36:52 +0100

    Home page for desktop is done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,31 +3,51 @@ import { Link, Routes, Route } from 'react-router-dom';
 
 import Menu from './Menu';
 import Delivery from './Delivery';
-//import Contact from './Contact';
+import Contact from './Contact';
 
 import Logo from './../images/pizzalogo.png';
 
 import SideBar  from './../components/SideMenu';
+import { dividerClasses } from '@mui/material';
 
 
 export default function Home() {
     return (
         <div>
             <SideBar pageWrapId={'page-wrap'} outerContainerId={'outer-container'}></SideBar>
-            <main className="content-div" id="front-page">
-                
-                <figure id="logo-front">
+            <main  id="front-page">
+                <div id="front-top">
+                    <figure id="logo-front">
                     <img src={Logo} alt="Pizza logo"></img>
                 </figure>
                 <h1>Pizzeria Bella</h1>
-                <div>
-                    <Link to="/Menu" className="link">Se menyen</Link>
-                    <Link to="/Delivery" className="link">Leveringsområder</Link>
                 </div>
+
+                <div id="front-page-div">
+                    <div className="content-div">
+                        <Link to="/Menu" className="link">Se menyen</Link>
+                        <Link to="/Delivery" className="link">Leveringsområder</Link>
+                        <Link to="/Contact" className="link">Kontakt oss</Link>
+                    </div>
+
+                    <div className="front-news">
+                        <h4>Få gratis levering på 
+                            første bestilling</h4>
+                        <h5>Bruk rabattkode <strong>BELLA</strong></h5>
+                        <div>
+                            <p>.</p>
+                            <p>.</p>
+                            <p>.</p>
+                        </div>
+                    </div>
+                </div>
+                
+                
 
                 <Routes>
                     <Route path="/Menu" element={<Menu/>}/>
                     <Route path="/Delivery" element={<Delivery/>}/>
+                    <Route path="/Contact" element={<Contact/>}/>
                 </Routes>
             </main>
         </div>
